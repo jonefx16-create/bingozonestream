7aa2172ee61ca331722212ce047f8d2245bf9ef5
jonefx16-create <jonefx16@gmail.com>
2026-06-14 02:50:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static(__dirname));
 // ==========================================
 // 🔵 DATABASE CONNECTION
 // ==========================================
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://bingostream:Jo97BJbi_0422/@!cluster0.hefpgl6.mongodb.net/BingoDB?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://bingostream:Jo97BJbi0422@cluster0.hefpgl6.mongodb.net/BingoDB?retryWrites=true&w=majority";
 mongoose.connect(mongoURI, { autoIndex: true, maxPoolSize: 500 }).then(() => console.log("✅ Database Connected")).catch(err => console.log("DB Error:", err.message));
 
 // ==========================================
